7da9aea5db0f35a62affa36564f80732d9f3bb26
akshay77-tech <akshay.040614@outlook.com>
2020-08-20 19:07:10 +0530

    Create app_pool.ps1

---
+++ PartsUnlimited-aspnet45/app_pool.ps1
@@ -0,0 +1,26 @@
+﻿param ($pool_name, $app_name, $workspace, $packagepath )
+
+#navigate to the app pools root
+cd IIS:\AppPools\
+
+#check if the app pool exists
+if (!(Test-Path $pool_name -pathType container))
+{
+    #create the app pool
+    New-WebAppPool -Name $pool_name -Force
+}
+else {
+    echo "Pool exists"
+}
+
+#navigate to the sites root
+cd IIS:\Sites\
+
+#check if the site exists
+if (!(Test-Path $app_name -pathType container))
+{
+   New-Website -Name $app_name -ApplicationPool $iisAppPoolName -Force -PhysicalPath $workspace\webapp\$packagepath -Port 8083
+}
+else {
+    echo "site exists"
+}
